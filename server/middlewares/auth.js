8bc4f5b979bb22c8fa9c81e28af9a78f9773871a
IanAcosta7 <ianacosta700@gmail.com>
2020-07-30 01:28:50 -0300

    Cookies bugfix in auth.js

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -6,27 +6,18 @@ if (process.env.NODE_ENV !== 'production')
 
 const verifyToken = (req, res, next) => {
 
-    const cookies = req.header('Cookie').split(' ').map(cookie => cookie.split("="));
-    const token = cookies.find(cookie => cookie[0] === 'jwt');
+    if (req.header('Cookie')) {
+        const cookies = req.header('Cookie').split(' ').map(cookie => cookie.split("="));
+        const token = cookies.find(cookie => cookie[0] === 'jwt');
 
-    if (token && token.length > 1) {
-        jwt.verify(token[1], process.env.JWT_SEED, (err, decoded) => {
-            if (!err)
-                req.user = decoded.user;
-        });
+        if (token && token.length > 1) {
+            jwt.verify(token[1], process.env.JWT_SEED, (err, decoded) => {
+                if (!err)
+                    req.user = decoded.user;
+            });
+        }
     }
-
-    // if (req.headers['authorization']) {
-    //     let token = req.headers['authorization'].split(' ')[1];      
-        
-    //     jwt.verify(token, process.env.JWT_SEED, (err, decoded) => {
     
-    //         if (!err)
-    //             req.user = decoded.user;
-            
-    //     });  
-    // }
-
     next();
 };
 
